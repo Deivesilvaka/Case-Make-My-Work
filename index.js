11c92d9d505563fbb6f34dd4554939ca3d5448e1
Deivesilvaka <luizdaviinformatica@gmail.com>
2020-11-16 15:18:14 -0300

    cors

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const app = require("express")()
 const bodyParser = require("body-parser")
+const cors = require("cors")
 
-app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); next(); });
-
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(require("./routes"))
